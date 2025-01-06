f0c2314389946e576b9302736ed44416810f7aee
Alex Larson <alexlarson011@gmail.com>
2025-01-06 08:50:11 -0600

    Add project files.

---
+++ myLearningApp/Program.cs
@@ -0,0 +1,47 @@
+﻿//Logical Operators
+// AND &&
+// OR ||
+// NOT !
+
+// Variants of OR statements
+// true || true -> true
+// true || false -> true
+// false || true -> true
+// false || false -> false
+
+// Variants of AND statements
+// true && true -> true
+// true && false -> false
+// false && true -> false
+// false && false -> false
+
+// relational operator < <= > >=
+
+
+byte age = 0;
+bool isWithParent = false;
+
+if(age >= 13 && age < 18)
+{
+    if (isWithParent)
+    {
+        Console.WriteLine("Go party in the club with your parents!");
+    }
+    else
+    {
+        Console.WriteLine("Go find a parent or go home!");
+    }
+}
+else if (age >= 18)
+{
+    Console.WriteLine("Go party in the club!");
+}
+else
+{
+    Console.WriteLine("You are too young to party in the club!");
+}
+
+Console.ReadKey();
+
+
+
